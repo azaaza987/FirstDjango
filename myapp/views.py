47479ec813090b43ff2b8b8199ff4b6493eecd83
Shawn <azaaza987@gmail.com>
2018-11-18 15:40:34 +0800

    v1

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.shortcuts import render
+
+# Create your views here.
+
+# 添加index函数，用于返回index.html页面
+def index(request):
+    return render(request, 'index.html')
